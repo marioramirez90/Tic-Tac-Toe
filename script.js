7d2409903379f6c8f01ebb251eb67ea69c938b3b
marioramirez90 <mario-ramirez@hotmail.de>
2026-02-16 10:40:48 +0100

    add generateCircleSVG(

--- script.js
+++ script.js
@@ -1,10 +1,10 @@
-let fields =[
+let fields = [
     null,
     null,
+    'circle',
     null,
     null,
-    null,
-    null,
+    'cross',
     null,
     null,
     null,
@@ -21,7 +21,7 @@ function render() {
             let symbol = "";
 
             if (fields[index] === "circle") {
-                symbol = "O";
+                symbol = generateCircleSVG();
             }
 
             if (fields[index] === "cross") {
@@ -38,3 +38,49 @@ function render() {
 
     document.getElementById("container").innerHTML = html;
 }
+
+function generateCircleSVG() {
+    return `
+        <svg width="70" height="70" viewBox="0 0 100 100">
+
+            <!-- Füll-Kreis -->
+            <circle 
+                cx="50" 
+                cy="50" 
+                r="40"
+                fill="#00b0ef"
+                transform="scale(0)"
+                transform-origin="50 50">
+                
+                <animateTransform
+                    attributeName="transform"
+                    type="scale"
+                    from="0"
+                    to="1"
+                    dur="0.6s"
+                    fill="freeze" />
+            </circle>
+
+            <!-- Outline -->
+            <circle 
+                cx="50" 
+                cy="50" 
+                r="40"
+                stroke="#00b0ef"
+                stroke-width="8"
+                fill="none"
+                opacity="0">
+                
+                <animate
+                    attributeName="opacity"
+                    from="0"
+                    to="1"
+                    begin="0.6s"
+                    dur="0.2s"
+                    fill="freeze" />
+            </circle>
+
+        </svg>
+    `;
+}
+
